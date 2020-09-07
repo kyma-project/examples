e8175c63b43089a002bdfb122a3cc19f0488dcbe
Michał Hudy <michal@hudy.pl>
2020-09-07 11:38:54 +0200

    Add orders-service example (#162)

---
+++ orders-service/internal/store/store.go
@@ -0,0 +1,19 @@
+package store
+
+import (
+	"context"
+	"errors"
+)
+
+type Store interface {
+	Add(ctx context.Context, key, value string) error
+	Get(ctx context.Context, key string) (string, error)
+	Keys(ctx context.Context, pattern string) ([]string, error)
+	Delete(ctx context.Context, key string) error
+	Clear(ctx context.Context) error
+}
+
+var (
+	NotFoundError      = errors.New("object not found")
+	AlreadyExistsError = errors.New("object already exists")
+)
