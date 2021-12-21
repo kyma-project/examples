52a8b6434607b64951907d9f5e6adca7deda6465
Karaluch <grzegorz.karaluch@sap.com>
2021-12-21 13:36:13 +0100

    examplary code

--- incluster_eventing/receiver/handler.js
+++ incluster_eventing/receiver/handler.js
@@ -1,5 +1,11 @@
 module.exports = {
     main: function (event, context) {
-        return 'Hello Serverless'
+        store(event.data)
+        return 'Stored'
     }
+}
+let store = (data)=>{
+    console.log(`storing data...`)
+    console.log(data)
+    return data
 }
