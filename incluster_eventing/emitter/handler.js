66e771fb047489776a45d8fae67d0153963f2bfa
Karaluch <grzegorz.karaluch@sap.com>
2022-01-03 09:51:15 +0100

    code refactor

--- incluster_eventing/emitter/handler.js
+++ incluster_eventing/emitter/handler.js
@@ -1,6 +1,17 @@
+const { v4: uuidv4 } = require('uuid');
 module.exports = {
     main: function (event, context) {
-        console.log(event.data)
-        return 'Hello Serverless'
+        let sanitisedData = sanitise(event.data)
+        var eventOut=event.buildResponseCloudEvent(uuidv4(),"sap.kyma.custom.acme.payload.sanitised.v1",sanitisedData);
+        eventOut.source="kyma"
+        eventOut.specversion="1.0"
+        event.publishCloudEvent(eventOut);
+        console.log(`Payload pushed to sap.kyma.custom.acme.payload.sanitised.v1`,eventOut)
+        return eventOut;
     }
+}
+let sanitise = (data)=>{
+    console.log(`sanitising data...`)
+    console.log(data)
+    return data
 }
