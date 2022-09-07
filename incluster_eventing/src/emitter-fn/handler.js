a381ffb6fc18681bfa51c75cfccc732fb2365cce
Krzysztof Kwiatosz <krzysztof.kwiatosz@sap.com>
2022-09-07 12:31:49 +0200

    Improve eventing example (#204)
    * wrap eventing call with custom span
    
    * change to nodejs16
    
    * Changes after code review
    
    * Apply suggestions from code review
    
    Co-authored-by: Grzegorz Karaluch <grzegorz.karaluch@sap.com>
    
    Co-authored-by: Grzegorz Karaluch <grzegorz.karaluch@sap.com>

--- incluster_eventing/src/emitter-fn/handler.js
+++ incluster_eventing/src/emitter-fn/handler.js
@@ -1,14 +1,40 @@
 const { v4: uuidv4 } = require('uuid');
-require('axios');
+const { SpanStatusCode } = require("@opentelemetry/api/build/src/trace/status");
+
 module.exports = {
-    main: function (event, context) {
+    main: async function (event, context) {
         let sanitisedData = sanitise(event.data)
-        var eventOut=event.buildResponseCloudEvent(uuidv4(),"sap.kyma.custom.acme.payload.sanitised.v1",sanitisedData);
-        eventOut.source="kyma"
-        eventOut.specversion="1.0"
-        event.publishCloudEvent(eventOut);
-        console.log(`Payload pushed to sap.kyma.custom.acme.payload.sanitised.v1`,eventOut)
-        return eventOut;
+
+        const msgId = uuidv4();
+        const eventType = "sap.kyma.custom.acme.payload.sanitised.v1";
+        const eventSource = "kyma";
+
+        var eventOut=event.buildResponseCloudEvent(msgId,eventType,sanitisedData);
+        eventOut.source=eventSource;
+        eventOut.specversion="1.0";
+
+        const span = event.tracer.startSpan('call-to-kyma-eventing');
+        return await event.publishCloudEvent(eventOut)
+            .then(resp => {
+                if(resp.status!==204){
+                    throw new Error("Unexpected response from eventing proxy");
+                }
+                span.addEvent("Event sent");
+                span.setAttribute("event-type", eventType);
+                span.setAttribute("event-source", eventSource);
+                span.setAttribute("event-id", msgId);
+                span.setStatus({code: SpanStatusCode.OK});
+                return "Event sent";
+            }).catch(err=> {
+                console.error(err)
+                span.setStatus({
+                    code: SpanStatusCode.ERROR,
+                    message: err.message,
+                  });
+                return err.message;
+            }).finally(()=>{
+                span.end();
+            });
     }
 }
 let sanitise = (data)=>{
