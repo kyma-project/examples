a5919754d22f1328f58b48d3c374850f667a5722
Krzysztof Kwiatosz <krzysztof.kwiatosz@sap.com>
2022-12-17 11:35:26 +0100

    Reflect changes in cloudevents api (#222)
    * Use new CE functions
    
    * reflect new api in documentation
    
    * changes after code review

--- incluster_eventing/src/emitter-fn/handler.js
+++ incluster_eventing/src/emitter-fn/handler.js
@@ -1,28 +1,27 @@
-const { v4: uuidv4 } = require('uuid');
 const { SpanStatusCode } = require("@opentelemetry/api/build/src/trace/status");
 
 module.exports = {
     main: async function (event, context) {
         let sanitisedData = sanitise(event.data)
 
-        const msgId = uuidv4();
         const eventType = "sap.kyma.custom.acme.payload.sanitised.v1";
         const eventSource = "kyma";
 
-        var eventOut=event.buildResponseCloudEvent(msgId,eventType,sanitisedData);
-        eventOut.source=eventSource;
-        eventOut.specversion="1.0";
+        //optional cloud event params
+        const eventtypeversion = "v1";
+        const datacontenttype = "application/json";
 
         const span = event.tracer.startSpan('call-to-kyma-eventing');
-        return await event.publishCloudEvent(eventOut)
+        
+        // you can pass additional cloudevents attributes  
+        // return await event.emitCloudEvent(eventType, eventSource, sanitisedData, {eventtypeversion, datacontenttype})
+        
+        return await event.emitCloudEvent(eventType, eventSource, sanitisedData)
             .then(resp => {
-                if(resp.status!==204){
-                    throw new Error("Unexpected response from eventing proxy");
-                }
+                console.log(resp.status);
                 span.addEvent("Event sent");
                 span.setAttribute("event-type", eventType);
                 span.setAttribute("event-source", eventSource);
-                span.setAttribute("event-id", msgId);
                 span.setStatus({code: SpanStatusCode.OK});
                 return "Event sent";
             }).catch(err=> {
@@ -30,7 +29,7 @@ module.exports = {
                 span.setStatus({
                     code: SpanStatusCode.ERROR,
                     message: err.message,
-                  });
+                });
                 return err.message;
             }).finally(()=>{
                 span.end();
