34f1c8ba068eb691d3acda6f800ba442a60bb28d
Stanislav Khalash <stanislav.khalash@sap.com>
2023-07-27 15:08:33 +0200

    Extend the sample app with a Histogram and a Summary (#251)
    * Add gitgnore
    
    * Add a histogram
    
    * Add a summary
    
    * Minor fix
    
    * Add a simple workflow
    
    * Move workflow
    
    * Some testing
    
    * Multi-stage build. Fix code
    
    * Adapt the workflow
    
    * Add login step
    
    * Add buildx step
    
    * Fix the step
    
    * Simplify tagging
    
    * Add short sha
    
    * Add permissions
    
    * Fix image name
    
    * Set the repository
    
    * Local repository
    
    * Remove workflow

--- prometheus/monitoring-custom-metrics/main.go
+++ prometheus/monitoring-custom-metrics/main.go
@@ -23,12 +23,28 @@ var (
 		},
 		[]string{"device"},
 	)
+	cpuEnergy = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name:    "cpu_energy_watt",
+			Help:    "Current power usage reported by the CPU.",
+			Buckets: prometheus.LinearBuckets(0, 20, 5),
+		},
+		[]string{"core"})
+	hwHumidity = prometheus.NewSummaryVec(
+		prometheus.SummaryOpts{
+			Name:       "hw_humidity",
+			Help:       "The summary of humidity rate reported by a humidity sensor, as a ratio.",
+			Objectives: map[float64]float64{0.5: 0.05, 0.9: 0.01, 0.99: 0.001},
+		},
+		[]string{"sensor"})
 )
 
 func init() {
 	// Metrics have to be registered to be exposed:
 	prometheus.MustRegister(cpuTemp)
 	prometheus.MustRegister(hdFailures)
+	prometheus.MustRegister(cpuEnergy)
+	prometheus.MustRegister(hwHumidity)
 }
 
 // randomTemp generates the temperature ranging from 60 to 90
@@ -36,8 +52,20 @@ func randomTemp() float64 {
 	return math.Round(rand.Float64()*300)/10 + 60
 }
 
+// randomEnergy generates the energy ranging from 0 to 100
+func randomEnergy() float64 {
+	return math.Round(rand.Float64() * 100)
+}
+
+// randomHumidity generates the humidity from 0 to 1
+func randomHumidity() float64 {
+	return rand.Float64()
+}
+
 func main() {
 	hdFailures.With(prometheus.Labels{"device": "/dev/sda"}).Inc()
+	cpuEnergy.With(prometheus.Labels{"core": "0"}).Observe(randomEnergy())
+	hwHumidity.With(prometheus.Labels{"sensor": "0"}).Observe(randomHumidity())
 
 	// The Handler function provides a default handler to expose metrics
 	// via an HTTP server. "/metrics" is the usual endpoint for that.
